f9f3e9eb0fd16fbfc1bec82d6bb1753ddcc51d57
Bruno Lagoa <miroldols@gmail.com>
2019-04-04 15:47:47 -0300

    - Projeto de API feito em AdonoisJS

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const Database = use('Database')
+const User = use('App/Models/User')
+
+class UserController {
+  async index () {
+    const users = await User.all()
+
+    return users
+  }
+
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
+
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(addresses, trx)
+
+    await trx.commit()
+
+    return user
+  }
+}
+
+module.exports = UserController
---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+
+    await Mail.send(
+      ['emails.new_task'],
+      {
+        username,
+        title,
+        hasAttachement: !!file
+      },
+      message => {
+        message
+          .to(email)
+          .from('bruno@gmail.com', 'Bruno Castro')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
